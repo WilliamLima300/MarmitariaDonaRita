10ea00f6e390d74d6a3a7b16a79796e813c3fcad
William Lima Alves <96958889+WilliamLima300@users.noreply.github.com>
2022-08-10 13:22:25 -0300

    Add files via upload

---
+++ dados.php
@@ -0,0 +1,13 @@
+<?php
+
+   $cardapio = array(
+    0=>array('id'=>'1','nome'=>'Picadinho de carne com fritas','tamanho'=>'Médio(1 Pessoa)','preco'=>'R$ 20,00','ingredientes'=>'Arroz,Chã de dentro frita em legumes e molho shoyo,batata frita.','imagem'=>'./images/cardapio1.png'),
+    1=>array('id'=>'2','nome'=>'Carne picada com camarão','tamanho'=>'Médio(1 Pessoa)','preco'=>'R$ 22,00','ingredientes'=>'Arroz,Chã de dentro frita em legumes,Camarão e molho shoyo,batata frita.','imagem'=>'./images/cardapio2.png'),
+    2=>array('id'=>'3','nome'=>'Farofa especial Dona Rita','tamanho'=>'Médio(1 Pessoa)','preco'=>'R$ 18,00','ingredientes'=>'Arroz,Farofa temperada especial ovo cozido e feijão.','imagem'=>'./images/cardapio3.png'),
+    3=>array('id'=>'4','nome'=>'Frango empanado com salada','tamanho'=>'Médio(1 Pessoa)','preco'=>'R$ 20,00','ingredientes'=>'Arroz,Frango empanado frito e vegetais variados,batata frita.','imagem'=>'./images/cardapio4.png'),
+    4=>array('id'=>'5','nome'=>'Feijoada especial Dona Rita','tamanho'=>'Médio(1 Pessoa)','preco'=>'R$ 24,00','ingredientes'=>'Arroz,Feijoada tradicional,vinagrete e couve.','imagem'=>'./images/cardapio5.png'),
+    5=>array('id'=>'6','nome'=>'Sobremesa: BROWNIE','tamanho'=>'Pequeno(1 Pessoa)','preco'=>'R$ 7,00','ingredientes'=>'Chocolate,manteiga, Açucar, farinha de trigo e ovos.','imagem'=>'./images/cardapio6.png'),
+    6=>array('id'=>'7','nome'=>'Sobremesa: Cheesecake','tamanho'=>'Pequeno(1 Pessoa)','preco'=>'R$ 12,00','ingredientes'=>'Creamcheese,farinha,ovos,açucar,frutas vermelhas.','imagem'=>'./images/cardapio7.png'),
+    7=>array('id'=>'8','nome'=>'Sobremesa: Pudim','tamanho'=>'Pequeno(1 Pessoa)','preco'=>'R$ 8,00','ingredientes'=>'Leite condensado, ovos,leite,açucar.','imagem'=>'./images/cardapio8.png')); 
+    
+?>
